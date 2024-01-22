9ef91c18c6ab9c2fadaf0ffaf815de71232e9d93
mahesabagusr <mahesabagusraditya1@gmail.com>
2024-01-22 11:51:37 +0700

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Halo aku Mahes</Text>
+      <Text>Namamu siapa maniesss?</Text>
       <StatusBar style="auto" />
     </View>
   );
